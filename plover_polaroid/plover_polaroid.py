1a75e3fcf0f57621c2b3b6404ce264ba02f576cf
Stanley Sakai <stanleysakai@Stanleys-MBP.fios-router.home>
2019-08-22 01:37:22 -0400

    Working out the kinks.

--- plover_polaroid/plover_polaroid.py
+++ plover_polaroid/plover_polaroid.py
@@ -11,6 +11,7 @@ from plover.steno import Stroke
 from plover.translation import Translator, Translation
 from plover.gui_qt.tool import Tool
 from plover.gui_qt.utils import ToolBar
+from plover.formatting import RetroFormatter
 
 from escpos.printer import Usb
 from plover_polaroid.plover_polaroid_ui import Ui_PloverPolaroid
@@ -21,10 +22,6 @@ try:
     printer = Usb(0x0416, 0x5011, 0, 0x81, 0x03)
 except:
     print("There was an error connecting to the printer.")
-    
-def get_translation(translator: Translator):
-    return translator.get_state().translations
-
 
 class PloverPolaroid(Tool, Ui_PloverPolaroid):
     ''' Prints your steno notes and/or your realtime output to a generic thermal receipt printer. '''
@@ -38,33 +35,39 @@ class PloverPolaroid(Tool, Ui_PloverPolaroid):
         super(PloverPolaroid, self).__init__(engine)
         self.setupUi(self)
         self.tape.setFocus()
+        self.translations = engine._translator.get_state().translations
+        self.line = 0
 
         engine.signal_connect('config_changed', self.on_config_changed)
         engine.signal_connect('stroked', self.on_stroke)
+        
 
     def on_stroke(self, stroke: Stroke):
+        raw_steno = ""
         keys = stroke.steno_keys[:]
-        print(keys)
-        print(get_translation(stroke))
         
-        raw_steno = ''
-
+        formatted = RetroFormatter(self.translations)
+        
         for key in keys:
-            print(key)
             if (len(keys) == 2 and key.find("-") != -1):
                 raw_steno += key[0] + key[1].replace("-", "")
             elif (len(keys) > 2):
                 raw_steno += key.replace("-", "")
             else:
                 raw_steno += key
+        last_words = formatted.last_words(0)
+        
+        final_text = "[" + raw_steno + "]" + "\t\t" + last_words[self.line] or ""
         
         print(raw_steno)
-        self.tape.appendPlainText(raw_steno)
-        # print(translations)
-
+        print(keys)
+        print(last_words)
+        self.tape.appendPlainText(final_text) 
         if (printer):
-            printer.text(raw_steno)
+            printer.text(final_text) 
             printer.text("\n")
+        
+        self.line += 1
     
     def on_config_changed(self):
         print("config changed!")
